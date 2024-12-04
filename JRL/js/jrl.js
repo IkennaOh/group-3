11a54134ff75016cf76d390ba1c5fa61c6e1e3ee
Ikennaoh <Ojhiaeri@gmail.com>
2024-12-03 18:31:28 -0800

    fix

--- JRL/js/jrl.js
+++ JRL/js/jrl.js
@@ -5,11 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-<<<<<<< Updated upstream
-var marker = L.marker([37.00073751929527, -122.05722133695241]).addTo(map);
-
-=======
 L.marker([36.99, -122.06]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
->>>>>>> Stashed changes
