0510031922b11018f0fe656759304b6909c8579d
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-12-04 16:11:13 -0800

    Update pk.js

--- porterkresge/js/pk.js
+++ porterkresge/js/pk.js
@@ -5,6 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
+L.marker([36.997615, -122.06579]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
