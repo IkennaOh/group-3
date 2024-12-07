181ef0db19b0f1f10b84119672baca43533cedfd
linguinediNO <yalorenz@ucsc.edu>
2024-12-06 21:55:58 -0800

    search bar update

--- js/site.js
+++ js/site.js
@@ -8,6 +8,15 @@ function search() {
       { name: "Rachel Carson College", url: "rcc/index.html" },
       { name: "Oakes College", url: "oakes/index.html" },
       { name: "Porter College", url: "porterkresge/index.html" }, 
+      { name: "Baskin Arts", url: "baskinarts/index.html"},
+      { name: "Science Hill", url: "sciencehill/index.html"},
+      { name: "McHenry Library", url: "baskinarts/index.html"},
+      { name: "Crown College", url: "crown/index.html"},
+      { name: "Cowell College", url: "Cowell/index.html"},
+      { name: "John R Lewis College", url:"JRL/index.html"},
+      { name: "College Nine", url: "C9/index.html"},
+      { name: "Merrill College", url: "merrill/index.html"},
+      { name: "Stevenson College", url: "Stevenson/index.html"},
       ];
   
      // Iterate through the colleges array and find matching ones
