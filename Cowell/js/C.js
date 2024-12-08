6793579a0303de1aaa1a9b01fd93c15d1f18dc6e
linguinediNO <yalorenz@ucsc.edu>
2024-12-08 14:47:55 -0800

    stevenson pin update

--- Cowell/js/C.js
+++ Cowell/js/C.js
@@ -5,6 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([36.99437, -122.059271]).addTo(map)
+    .bindPopup('<div class="popupOne"> <strong> Most Laundry rooms </strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
     .openPopup();
--- Stevenson/js/S.js
+++ Stevenson/js/S.js
@@ -5,6 +5,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([36.996529, -482.051491]).addTo(map)
+    .bindPopup('<div class="popupOne"> <strong> House 5, Second Floor </strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
+    .openPopup();
+
+L.marker([36.997212, -482.052484]).addTo(map)
+    .bindPopup('<div class="popupTwo"> <strong> Stevenson Game Room, Located between the Stevenson Cafe and the Stevenson Programming Office. </strong> <br>  Type: Snacks & Beverages <br> Rating: 2 out of 5 <br> </div>')
     .openPopup();
