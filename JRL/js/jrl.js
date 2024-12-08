56ddc587cb23eba0901a1ef3458383a8f1a34720
linguinediNO <yalorenz@ucsc.edu>
2024-12-08 12:39:23 -0800

    jrl js. pin updates

--- JRL/js/jrl.js
+++ JRL/js/jrl.js
@@ -5,6 +5,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([37.001683, -842.059132]).addTo(map)
+    .bindPopup('<div class="popupOne"> <strong> Social Sciences 2 </strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
+    .openPopup();
+
+L.marker([37.003354, -842.058537]).addTo(map)
+    .bindPopup('<div class="popupThree"> <strong> International Living Center, Laundry room (behind Social Sciences 1) </strong> <br> Type: Snacks & Beverages <br> Rating: 3 out of 5 <br> </div>')
+    .openPopup();
+
+L.marker([37.000621, -842.057805]).addTo(map)
+    .bindPopup('<div class="popupThree"> <strong> Recreation Room underneath the Dining Hall (requires a 9/JRL ID to enter) (lie the dorms) </strong> <br> Type: Snacks & Beverages <br> Rating: 3 out of 5 <br> </div>')
+    .openPopup();
+
+L.marker([37.002666, -842.058859]).addTo(map)
+    .bindPopup('<div class="popupFour"> <strong> College JRL & Nine Aparment, Laundry room behind Social Sciences 2 (cash only) </strong> <br> Type: Snacks & Beverages <br> Rating: 3 out of 5 <br> </div>')
     .openPopup();
