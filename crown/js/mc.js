1530fd20459e9ff448384b0d03eae03a79ef80d8
linguinediNO <yalorenz@ucsc.edu>
2024-12-08 14:16:43 -0800

    crown/merrill pin update

--- crown/js/mc.js
+++ crown/js/mc.js
@@ -5,6 +5,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([37.001799, -482.05299]).addTo(map)
+    .bindPopup('<div class="popupOne"> <strong>Merrill/Crown Apartments, Laundry room <strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
+    .openPopup();
+
+L.marker([37.000119, -482.054541]).addTo(map)
+    .bindPopup('<div class="popupTwo"> <strong>Fireside Lounge<strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
     .openPopup();
--- merrill/js/mc.js
+++ merrill/js/mc.js
@@ -1,10 +1,15 @@
-var map = L.map('map').setView([36.99, -122.06], 13);
+var map = L.map('map').setView([35.99, -122.06], 18);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([36.99, -122.06]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([36.999986, -482.052754]).addTo(map)
+    .bindPopup('<div class="popupOne"> <strong>Laundry room of "B" dorm <strong> Social Sciences 2 </strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
     .openPopup();
+
+L.marker([37.001799, -482.05299]).addTo(map)
+    .bindPopup('<div class="popupTwo"> <strong>Merrill/Crown Apartments, Laundry room <strong> Social Sciences 2 </strong> <br>  Type: Snacks & Beverages <br> Rating: 4 out of 5 <br> </div>')
+    .openPopup();
+
